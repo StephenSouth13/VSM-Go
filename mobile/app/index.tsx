80255c12c2fdc80e5b8e2a90d08bcb4f177cceba
Quách Thành Long <stephensouth1307@gmail.com>
2025-08-13 12:16:49 +0700

    -update

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome NguyenDung0101!</Text>
+      <Text>Welcome VSM GO APP</Text>
     </View>
   );
 }
